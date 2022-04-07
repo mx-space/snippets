922e0912d24387c3cde3d40f041c67ea71bdf0a9
Innei <tukon479@gmail.com>
2022-04-07 21:31:15 +0800

    feat(kami): update snippet

--- kami/functions/netease.ts
+++ kami/functions/netease.ts
@@ -1,23 +1,32 @@
 import extra from '@mx-space/extra'
 
 async function handler() {
-  const { NeteaseMusic } = extra
+  const { NeteaseMusic, NeteaseCloudMusicApi } = extra
 
   const client = new NeteaseMusic(phone, password)
   await client.Login()
+
+  const uid = await client.getAccount()
 
   const weekdata = await client.getWeekData()
   const alldata = await client.getAllData()
   const playlist = await client.getFavorite()
 
+  const detail = await NeteaseCloudMusicApi.user_detail({
+    uid,
+  }).then((res) => (res.body.code === 200 ? res.body.profile : null))
+
   const responsePayload = {
     playlist,
     weekdata,
     alldata,
+    detail,
   }
 
   return responsePayload
 }
 
+/// CONFIGS ///
 const phone = ''
 const password = ''
+/// CONFIGS END ///
