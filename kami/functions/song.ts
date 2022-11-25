3882aa9bae7352c33838e474cfb626dbff7551d3
Innei <tukon479@gmail.com>
2022-11-25 22:15:01 +0800

    fix: update song
    Signed-off-by: Innei <tukon479@gmail.com>

--- kami/functions/song.ts
+++ kami/functions/song.ts
@@ -1,14 +1,35 @@
-import { NeteaseCloudMusicApi } from '@mx-space/extra'
+type Song = {
+  author: string
+  /**
+   * 封面 url
+   */
+  pic: string
+  title: string
 
-async function handler() {
-  const { song_url } = NeteaseCloudMusicApi
-  const id = context.req.query.id
+  url: string
+}
+
+async function handler(ctx, require) {
+  const { NeteaseCloudMusicApi } = await require('@mx-space/extra')
+  const { song_url, song_detail } = NeteaseCloudMusicApi
+  const id = ctx.req.query.id
   if (!id) {
     return { message: 'id must be not empty stringnumber' }
   }
-  const data = await song_url({
-    id: +id,
-  })
-
-  return data.body.data
+  const [data, data2] = await Promise.all([
+    song_url({
+      id: +id,
+    }),
+    song_detail({
+      ids: id,
+    }),
+  ])
+  const song = data2.body.songs[0]
+  const songDetail: Song = {
+    title: song.name,
+    author: song.ar?.map((a) => a.name).join(' & '),
+    pic: song.al?.picUrl,
+    url: data.body.data?.[0]?.url,
+  }
+  return [songDetail]
 }
